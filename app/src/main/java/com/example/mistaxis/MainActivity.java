750a105436bab06a16cfec21179087e75c449c81
AlvaroQQ <alvaro19aef@gmail.com>
2022-12-21 17:56:39 -0400

    commit 21 de Diciembre

--- app/src/main/java/com/example/mistaxis/MainActivity.java
+++ app/src/main/java/com/example/mistaxis/MainActivity.java
@@ -2,18 +2,39 @@ package com.example.mistaxis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Prueba.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 5000);
+
     }
 }
