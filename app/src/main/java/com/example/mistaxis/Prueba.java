c34b587a9282ef39ea6d36dce9a9adccd85e0980
AlvaroQQ <alvaro19aef@gmail.com>
2023-01-25 11:12:50 -0400

    commit miercoles 25 enero 2023

--- app/src/main/java/com/example/mistaxis/Prueba.java
+++ app/src/main/java/com/example/mistaxis/Prueba.java
@@ -2,11 +2,12 @@ package com.example.mistaxis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Prueba extends AppCompatActivity {
+public class Prueba extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,11 @@ public class Prueba extends AppCompatActivity {
 
     public void irActividadRegistroTaxista(View vista){
         Intent intent = new Intent(Prueba.this,ActivityRegisterTaxista.class);
+        startActivity(intent);
+    }
+
+    public void irActividadLogin(View vista){
+        Intent intent = new Intent(Prueba.this, ActivityLogin.class);
         startActivity(intent);
     }
 }
